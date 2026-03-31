d981be509b21556fea61255fc122c114d509b5d5
Nicolas <nicolas.sepulveda103@inacapmail.cl>
2026-03-31 17:22:38 -0300

    version estable agregar favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <title>Registro de Mascotas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     @vite('resources/js/app.js')
-</head>
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 <body>
     <div id="app"></div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
--- storage/framework/views/087445b4bdca8f4f69168f3143b1075b.php
+++ storage/framework/views/087445b4bdca8f4f69168f3143b1075b.php
@@ -6,7 +6,7 @@
     <title>Registro de Mascotas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <?php echo app('Illuminate\Foundation\Vite')('resources/js/app.js'); ?>
-</head>
+    <link rel="icon" type="image/png" href="<?php echo e(asset('favicon.png')); ?>">
 <body>
     <div id="app"></div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
